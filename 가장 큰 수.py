c0d96dcd1185b3b2d7fd1645e2609037b0748089
JuJiwon <36442946+codingstr@users.noreply.github.com>
2022-01-19 23:32:13 +0900

    실패
    같은 자리값을 붙이는 방식
    a+b와 b+a를 비교해 a와 b를 바꾸는 알고리즘이 필요해 보임

--- 가장 큰 수.py
+++ 가장 큰 수.py
@@ -1,44 +1,31 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42746
 
-def sortSamePlaceValueNums(numbers) :
-
-
 def solution(numbers) :
 
     answer = ''
+    numbers.sort(reverse=True)
     numbers.sort(key= lambda num : int(str(num)[0]), reverse=True)
 
     if numbers.count(0)==len(numbers) :
         answer = '0'
     else :
-        # before
+        idx = 0
+        while idx < len(numbers)-1 :
+            if str(numbers[idx])[0] == str(numbers[idx+1])[0] :
+                samePlaceValueNums = [num for num in numbers[idx:] if str(num)[0]==str(numbers[idx])[0]]
+                maxNumLen = len(str(max(samePlaceValueNums)))
+                samePlaceValueNums.sort(key= lambda num : int(str(num)+str(numbers[idx])[0]*(maxNumLen-len(str(num)))), reverse=True)
+                sortedSamePlaceValueIdxs = [numbers.index(num) for num in samePlaceValueNums]
+                numbers = numbers[:min(sortedSamePlaceValueIdxs)] + samePlaceValueNums + numbers[max(sortedSamePlaceValueIdxs)+1:]
 
-        # idx = 0
-        # while idx < len(numbers)-1 :
-        #     if str(numbers[idx])[0] == str(numbers[idx+1])[0] :
-        #         samePlaceValue = int(str(numbers[idx])[0]) 
-        #         samePlaceValueIdxs = sorted([i+idx for i, num in enumerate(numbers[idx:]) if str(num)[0]==str(numbers[idx])[0]], key= lambda i : numbers[i], reverse=True)
-                
-        #         sortedSamePlaceValueNums = list()
-        #         for sortLen in set([len(str(num)) for num in sorted(numbers[min(samePlaceValueIdxs):max(samePlaceValueIdxs)+1], key= lambda num : len(str(num)))]) :
-        #             longerNums = [num for num in numbers[min(samePlaceValueIdxs):max(samePlaceValueIdxs)+1] if len(str(num))>=sortLen]
-        #             leftNums = sorted([num for num in longerNums if int(str(num)[sortLen-1])>=samePlaceValue], key= lambda num : int(str(num)[sortLen-1]), reverse=True)
-        #             rightNums = sorted(list(set(longerNums)-set(leftNums)), reverse=True)
-        #             print(longerNums, leftNums, rightNums)
-        #             sortedSamePlaceValueNums = leftNums + ([samePlaceValue] if samePlaceValue in numbers else []) + rightNums
-
-        #         numbers = numbers[:min(samePlaceValueIdxs)] + sortedSamePlaceValueNums + numbers[max(samePlaceValueIdxs)+1:]
-
-        #         idx = max(samePlaceValueIdxs)
-        #     idx += 1
-        # for num in numbers : answer += str(num)
-
-        # after
-        
-        numbers = sortSamePlaceValueNums(numbers)
-
+                idx += len(samePlaceValueNums)
+                continue
+            idx += 1
+        for num in numbers : answer += str(num)
 
     return answer
 
-print(solution([3, 30, 34, 5, 9])+", 9534330")
-print(solution([30, 3021])+", 303021")
+print(solution([979, 97, 978, 81, 818, 817]))
+print('9799797881881817')
+print(solution([67, 676, 677]))
+print('67767676')
